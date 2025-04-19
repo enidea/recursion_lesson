c97998747816495347d93dd2f124590a92918c07
eriku66 <eriku.hashimoto@gmail.com>
2025-04-20 01:05:13 +0900

    solve

---
+++ ts/410.ts
@@ -0,0 +1,42 @@
+class SinglyLinkedListNode<E> {
+  public data: E;
+  public next: SinglyLinkedListNode<E> | null;
+
+  constructor(data: E) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+const makeListFromHead = (head: SinglyLinkedListNode<number>) => {
+  const list: number[] = [head.data];
+  let current = head;
+
+  while (current.next !== null) {
+    current = current.next;
+    list.push(current.data);
+  }
+
+  return list;
+};
+
+function findMergeNode(
+  headA: SinglyLinkedListNode<number>,
+  headB: SinglyLinkedListNode<number>,
+): number {
+  const aList = makeListFromHead(headA);
+  const bList = makeListFromHead(headB);
+
+  aList.reverse();
+  bList.reverse();
+
+  let result = -1;
+  let index = 0;
+
+  while (aList[index] === bList[index]) {
+    result = aList[index];
+    index++;
+  }
+
+  return result;
+}
