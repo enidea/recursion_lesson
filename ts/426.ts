76349701b77812927fda6d2f839b5425db26c728
eriku66 <eriku.hashimoto@gmail.com>
2025-04-21 23:43:36 +0900

    solve

---
+++ ts/426.ts
@@ -0,0 +1,58 @@
+const calcResult = (diceRolls: number[]) => {
+  const GAIN = 4;
+
+  let result = 0;
+  let rolls: number[] = [];
+
+  for (let index = 0; index < diceRolls.length; index++) {
+    if ((diceRolls[index - 1] ?? 0) > diceRolls[index]) {
+      result = 0;
+      rolls = [];
+    }
+
+    result += GAIN;
+    rolls.push(diceRolls[index]);
+  }
+
+  return { result, rolls };
+};
+
+function diceStreakGamble(
+  player1: number[],
+  player2: number[],
+  player3: number[],
+  player4: number[],
+): string {
+  const players = { player1, player2, player3, player4 };
+  type Players = typeof players;
+  type PlayerName = keyof Players;
+
+  const winner: {
+    result: number;
+    name: PlayerName | undefined;
+    rolls: number[];
+  } = {
+    result: 0,
+    name: undefined,
+    rolls: [],
+  };
+
+  for (const [playerName, diceRolls] of Object.entries(players) as [
+    PlayerName,
+    Players[PlayerName],
+  ][]) {
+    const result = calcResult(diceRolls);
+
+    if (result.result > winner.result) {
+      winner.result = result.result;
+      winner.name = playerName;
+      winner.rolls = result.rolls;
+    }
+  }
+
+  if (!winner.name) {
+    throw new Error('No winner');
+  }
+
+  return `Winner: Player ${winner.name[winner.name.length - 1]} won $${winner.result} by rolling [${winner.rolls}]`;
+}
