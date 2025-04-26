cfcba58d2e35e3ac93cda3167cf4bbd7b7380e4f
eriku66 <eriku.hashimoto@gmail.com>
2025-04-26 11:51:09 +0900

    refactor stockSpan to optimize span calculation and improve readability

--- ts/427.ts
+++ ts/427.ts
@@ -1,17 +1,14 @@
 function stockSpan(stocks: number[]): number[] {
   const stack: number[] = [];
-  const spans: number[] = [];
+  const spans: number[] = new Array(stocks.length).fill(0);
 
   stocks.forEach((stock, index) => {
     while (stack.length > 0 && stock > stocks[stack[stack.length - 1]]) {
       stack.pop();
     }
 
-    if (stack.length === 0) {
-      spans.push(index + 1);
-    } else {
-      spans.push(index - stack[stack.length - 1]);
-    }
+    spans[index] =
+      stack.length === 0 ? index + 1 : index - stack[stack.length - 1];
 
     stack.push(index);
   });
