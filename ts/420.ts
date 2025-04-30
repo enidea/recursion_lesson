bd32bc285dfbf3eee76fa05b498b14f03103db17
eriku66 <eriku.hashimoto@gmail.com>
2025-05-01 02:36:48 +0900

    refactor largestRectangle and calculateLeftSpans for improved readability and logic optimization

--- ts/420.ts
+++ ts/420.ts
@@ -1,49 +1,35 @@
+const calculateLeftSpans = (numbers: number[]) => {
+  const spans = new Array<number>(numbers.length);
+  const indexStack = new Array<number>();
+
+  for (let index = 0; index < numbers.length; index++) {
+    const currentNumber = numbers[index];
+    let span = 1;
+
+    while (
+      indexStack.length > 0 &&
+      currentNumber <= numbers[indexStack[indexStack.length - 1]]
+    ) {
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      span += spans[indexStack.pop()!];
+    }
+
+    indexStack.push(index);
+    spans[index] = span;
+  }
+
+  return spans;
+};
+
 function largestRectangle(heights: number[]): number {
-  const lefts = new Array<number>(heights.length);
-  const stackForLeft = new Array<number>();
+  const lefts = calculateLeftSpans(heights);
+  const rights = calculateLeftSpans([...heights].reverse()).reverse();
+
+  const rectangles: number[] = [];
 
   for (let index = 0; index < heights.length; index++) {
-    const height = heights[index];
-    let left = 1;
-
-    while (
-      stackForLeft.length > 0 &&
-      height <= heights[stackForLeft[stackForLeft.length - 1]]
-    ) {
-      left += lefts[stackForLeft.pop()!];
-    }
-
-    stackForLeft.push(index);
-    lefts[index] = left;
+    rectangles.push(heights[index] * (lefts[index] + rights[index] - 1));
   }
 
-  const rights = new Array<number>(heights.length);
-  const stackForRight = new Array<number>();
-
-  for (let index = heights.length - 1; index >= 0; index--) {
-    const height = heights[index];
-    let right = 1;
-
-    while (
-      stackForRight.length > 0 &&
-      height <= heights[stackForRight[stackForRight.length - 1]]
-    ) {
-      right += rights[stackForRight.pop()!];
-    }
-
-    stackForRight.push(index);
-    rights[index] = right;
-  }
-
-  let result = 0;
-
-  for (let index = 0; index < heights.length; index++) {
-    const rectangle = heights[index] * (lefts[index] + rights[index] - 1);
-
-    if (rectangle > result) {
-      result = rectangle;
-    }
-  }
-
-  return result;
+  return Math.max(...rectangles);
 }
