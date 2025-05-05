0551fefd171fdc57e1f371bef399c95ee192dedd
eriku66 <eriku.hashimoto@gmail.com>
2025-05-05 23:21:44 +0900

    solve

---
+++ ts/477.ts
@@ -0,0 +1,50 @@
+class BinaryTree<E> {
+  data: E;
+  left: BinaryTree<E> | null;
+  right: BinaryTree<E> | null;
+
+  constructor(
+    data: E,
+    left: BinaryTree<E> | null = null,
+    right: BinaryTree<E> | null = null,
+  ) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function bstInsert(
+  root: BinaryTree<number> | null,
+  key: number,
+): BinaryTree<number> | null {
+  const newNode = new BinaryTree(key);
+
+  if (!root) {
+    return newNode;
+  }
+
+  let current: BinaryTree<number> | null = root;
+
+  while (current) {
+    if (current.data === key) {
+      break;
+    }
+
+    if (current.data > key) {
+      if (current.left) {
+        current = current.left;
+      } else {
+        current.left = newNode;
+        break;
+      }
+    } else if (current.right) {
+      current = current.right;
+    } else {
+      current.right = newNode;
+      break;
+    }
+  }
+
+  return root;
+}
